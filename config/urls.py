64982f67207b8f94acb8fcd2cfe3d851c05cddae
JHeaon <j3heawon@gmail.com>
2023-08-28 14:19:29 +0900

    Docs: spactaclular을 이용한 Swagger 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularAPIView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     path("admin/", admin.site.urls),
+    path("account/", include("accounts.urls")),
+    path("api/", include("api.urls")),
 ]
